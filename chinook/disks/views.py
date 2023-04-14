8aa3d5848d316ef97c5561930e12150ce847b717
jlabaisse <jlabaisse@laas.fr>
2023-04-14 14:03:38 +0200

    add home and detail views

--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404, get_list_or_404
+from .models import *
 
-# Create your views here.
+# Home page
+def album_list(request):
+    albums = get_list_or_404(Album)
+    return render(request, 'disks/album_list.html', {'albums': albums})
+
+# Detail page, specific to the album
+def album_details(request, albumId):
+    album = get_object_or_404(Album, pk=albumId)
+    return render(request, 'disks/album_details.html', {'album': album})
